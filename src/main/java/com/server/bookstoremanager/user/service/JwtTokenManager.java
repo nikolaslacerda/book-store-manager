c578d0aab0bc1bee12355765fabf1332e44265b6
nikolaslacerda <nikolas.lacerda@hotmail.com>
2021-03-15 15:07:50 -0300

    add jwt request filter class

--- src/main/java/com/server/bookstoremanager/user/service/JwtTokenManager.java
+++ src/main/java/com/server/bookstoremanager/user/service/JwtTokenManager.java
@@ -60,7 +60,7 @@ public class JwtTokenManager {
                 .getBody();
     }
 
-    private boolean validateToken(String token, UserDetails userDetails) {
+    public boolean validateToken(String token, UserDetails userDetails) {
         String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
